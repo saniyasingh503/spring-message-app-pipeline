71c4cbf1c0dd59ddd373fe3a4b28cd698a8236f8
saniyasingh <saniya.singh@nagarro.com>
2024-11-28 11:38:02 +0530

    modify start_container script

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Run the Docker container
-docker run -d --name message-app-container -p 80:80 123456789012.dkr.ecr.us-east-1.amazonaws.com/my-repository:latest
+docker run -d --name message-app-container -p 80:80 886436972291.dkr.ecr.ap-south-1.amazonaws.com/spring-message-app:latest
